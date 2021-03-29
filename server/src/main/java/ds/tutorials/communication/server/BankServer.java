c44399c2017575aa586396e658b8a82f578cb2e0
murshidazher <murshidazher@yahoo.com>
2021-03-29 22:59:21 +0530

    :sparkles: feat: generate the executable fat jar

--- server/src/main/java/ds/tutorials/communication/server/BankServer.java
+++ server/src/main/java/ds/tutorials/communication/server/BankServer.java
@@ -6,9 +6,10 @@ import io.grpc.ServerBuilder;
 public class BankServer {
   public static void main (String[] args) throws Exception{
     int serverPort = 11436;
+    // we can use server builder to create the server
     Server server = ServerBuilder
       .forPort(serverPort)
-      .addService(new BalanceServiceImpl())
+      .addService(new BalanceServiceImpl()) // host my check balance services with this implementation in the port
       .build();
     server.start();
     System.out.println("BankServer Started and ready to accept requests on port " + serverPort);
