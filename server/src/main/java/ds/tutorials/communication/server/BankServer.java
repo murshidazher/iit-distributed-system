c992989a24fe769a3f782f3ba48d976dae0c7f51
murshidazher <murshidazher@yahoo.com>
2021-03-31 00:05:34 +0530

    :package: build: build the server

--- server/src/main/java/ds/tutorials/communication/server/BankServer.java
+++ server/src/main/java/ds/tutorials/communication/server/BankServer.java
@@ -1,17 +1,28 @@
 package ds.tutorials.communication.server;
 
+import ds.tutorials.name.service.client.NameServiceClient;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 public class BankServer {
+  public static final String NAME_SERVICE_ADDRESS = "http://localhost:2379";
+
   public static void main (String[] args) throws Exception{
-    int serverPort = 11436;
+    if (args.length != 1) {
+      System.out.println("Please have the port as an argument");
+      System.exit(1);
+    }
+
+    int serverPort = Integer.parseInt(args[0]);
     // we can use server builder to create the server
     Server server = ServerBuilder
       .forPort(serverPort)
       .addService(new BalanceServiceImpl()) // host my check balance services with this implementation in the port
       .build();
     server.start();
+    NameServiceClient client = new NameServiceClient(NAME_SERVICE_ADDRESS);
+    client.registerService("CheckBalanceService", "127.0.0.1", serverPort, "tcp");
+
     System.out.println("BankServer Started and ready to accept requests on port " + serverPort);
     server.awaitTermination();
   }
