ffc1b57f301e714f55d42536917aa124181cd1cd
murshidazher <murshidazher@yahoo.com>
2021-04-11 01:15:46 +0530

    :sparkles: feat: change server to get leader data

--- server/src/main/java/ds/tutorials/communication/server/BankServer.java
+++ server/src/main/java/ds/tutorials/communication/server/BankServer.java
@@ -1,29 +1,90 @@
 package ds.tutorials.communication.server;
 
 import ds.tutorials.name.service.client.NameServiceClient;
+import ds.tutorials.synchronization.lock.DistributedLock;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.apache.zookeeper.KeeperException;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class BankServer {
-  public static final String NAME_SERVICE_ADDRESS = "http://localhost:2379";
+  private int serverPort;
+  private DistributedLock leaderLock;
+  private AtomicBoolean isLeader = new AtomicBoolean(false);
+  private byte[] leaderData;
+
+  public BankServer(String host, int port) throws InterruptedException, IOException, KeeperException {
+    this.serverPort = port;
+    leaderLock = new DistributedLock("BankServerTestCluster", buildServerData(host, port));
+  }
+
+  public static String buildServerData(String IP, int port) {
+    StringBuilder builder = new StringBuilder();
+    builder.append(IP).append(":").append(port);
+    return builder.toString();
+  }
+
+  public void startServer() throws IOException, InterruptedException {
+    Server server = ServerBuilder
+      .forPort(serverPort)
+      .addService(new BalanceServiceImpl())
+      .build();
+    server.start();
+    System.out.println("BankServer Started and ready to accept requests on port " + serverPort);
+
+    server.awaitTermination();
+  }
+
+  // to check if the current server is the leader
+  public boolean isLeader() {
+    return isLeader.get();
+  }
+
+  // if its the leader set the data
+  private synchronized void setCurrentLeaderData(byte[] leaderData) {
+    this.leaderData = leaderData;
+  }
 
   public static void main (String[] args) throws Exception{
     if (args.length != 1) {
-      System.out.println("Please have the port as an argument");
-      System.exit(1);
+      System.out.println("Usage executable-name <port>");
     }
 
     int serverPort = Integer.parseInt(args[0]);
-    // we can use server builder to create the server
-    Server server = ServerBuilder
-      .forPort(serverPort)
-      .addService(new BalanceServiceImpl()) // host my check balance services with this implementation in the port
-      .build();
-    server.start();
-    NameServiceClient client = new NameServiceClient(NAME_SERVICE_ADDRESS);
-    client.registerService("CheckBalanceService", "127.0.0.1", serverPort, "tcp");
+    DistributedLock.setZooKeeperURL("localhost:2181");
 
-    System.out.println("BankServer Started and ready to accept requests on port " + serverPort);
-    server.awaitTermination();
+    BankServer server = new BankServer("localhost", serverPort);
+    server.startServer();
+  }
+
+  //  A separate thread implementation which continuously tries to acquire the lock.
+  //  While this lock is busy with election main thread can continue to serve the requests.
+  class LeaderCampaignThread implements Runnable {
+    private byte[] currentLeaderData = null;
+
+    @Override
+    public void run() {
+      System.out.println("Starting the leader Campaign");
+      try {
+        boolean leader = leaderLock.tryAcquireLock();
+        while (!leader) {
+          byte[] leaderData = leaderLock.getLockHolderData();
+          if (currentLeaderData != leaderData) {
+            // if the current object isn't the leader then give the leaders data and save it
+            currentLeaderData = leaderData;
+            setCurrentLeaderData(currentLeaderData);
+          }
+          Thread.sleep(10000);
+          leader = leaderLock.tryAcquireLock();
+        }
+        System.out.println("I got the leader lock. Now acting as primary");
+        isLeader.set(true);
+        currentLeaderData = null;
+      } catch (Exception e) {
+        // throw the exception
+      }
+    }
   }
 }
